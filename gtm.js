128ef1446865beef267b6e6165b87a2df86f1c48
Nitish <nitish@fedora16lxde.com>
2012-12-05 00:46:09 +0530

    added movies.json and getJSON method (loads the last entry in the json file as movie)

--- gtm.js
+++ gtm.js
@@ -1,5 +1,5 @@
-var movie = "pursuit of happyness".split("");
-var hidden_movie = "******* ** *********".split("");
+var movie = null;
+var hidden_movie = null;
 var chances = 7;
 
 $(document).ready(function() {
@@ -14,9 +14,25 @@ $(document).ready(function() {
     });
     */
 
+    $.getJSON('./movies.json', function(data) {
+        $.each(data, function(key, val) {
+            var mov = key.toLowerCase();
+            movie = mov.split("");
+            hidden_movie = mov.replace(/[a-z]/g, "*").split("");
+        });
+        refreshDisplay();
+    });
+
     var refreshDisplay = function() {
         $('div#movie h3').html(movie.join("") + ' || ' + hidden_movie.join(""));
         $('div#chances h1').html(chances);
+    };
+
+    var endGame = function() {
+        $('#give-up').attr('disabled','disabled');
+        $('.alphabets').attr('disabled','disabled');
+        hidden_movie = movie;
+        refreshDisplay();
     };
 
     var useChance = function() {
@@ -49,24 +65,21 @@ $(document).ready(function() {
         $(this).attr('disabled','disabled');
         processCharacter(event.target.id);
         if(isWon()) {
+            endGame();
             alert("Jinklas ki re!");
         } else if(isLost()) {
+            endGame();
             alert("Harlas ki re!");
         }
     });
 
     $('#give-up').click(function() {
-        $(this).attr('disabled','disabled');
-        $('.alphabets').attr('disabled','disabled');
         chances = 0;
-        hidden_movie = movie;
-        refreshDisplay();
+        endGame();
     });
 
     $('#restart').click(function() {
         location.reload(true);
     });
-
-    refreshDisplay();
 
 });
